48e01e9aa5ed802e762904b9211e111f90e45520
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-17 06:27:00 -0300

    creando contenedor

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export const Order = () => {
-  return <section>Order</section>;
+  return (
+    <section className="container">
+      <div className="container__card">
+        <div className="container__text">
+          <h1> Order Summary</h1>
+          <p>
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+        </div>
+        <div className="container__price">
+          <p>Annual Plan</p>
+          <p> $59.99/year</p>
+          <ul>
+            <li>change</li>
+          </ul>
+        </div>
+        <div className="container__btn">
+          <button> Proceed to Payment</button>
+        </div>
+      </div>
+    </section>
+  );
 };
