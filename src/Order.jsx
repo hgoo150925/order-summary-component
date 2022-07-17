526e3edd2a6823c2de16d69152f6b451689d06ae
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-17 06:40:02 -0300

    añadiendo estilos al container_card--text

--- src/Order.jsx
+++ src/Order.jsx
@@ -4,11 +4,13 @@ export const Order = () => {
   return (
     <section className="container">
       <div className="container__card">
-        <h1> Order Summary</h1>
-        <p>
-          You can now listen to millions of songs, audiobooks, and podcasts on
-          any device anywhere you like!
-        </p>
+        <div className="container__card--text">
+          <h1> Order Summary</h1>
+          <p>
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+        </div>
 
         <div className="container__price">
           <p>Annual Plan</p>
