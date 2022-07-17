29404af2555415dc9407d172a9ba84128035ea42
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-17 06:34:51 -0300

    añadiendo posiciones al container_card

--- src/Order.jsx
+++ src/Order.jsx
@@ -4,13 +4,12 @@ export const Order = () => {
   return (
     <section className="container">
       <div className="container__card">
-        <div className="container__text">
-          <h1> Order Summary</h1>
-          <p>
-            You can now listen to millions of songs, audiobooks, and podcasts on
-            any device anywhere you like!
-          </p>
-        </div>
+        <h1> Order Summary</h1>
+        <p>
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+
         <div className="container__price">
           <p>Annual Plan</p>
           <p> $59.99/year</p>
